eeb98f7b5ad5ab966d07cad8ef41afdaa49473a6
Ben Green <ben@bcgdesign.com>
2022-03-22 14:19:25 +0000

    Updating build config - see #1

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 // Mileage Tracker
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
-var (_, _) = Jeebs.Apps.Host.Create(args);
+var (_, log) = Jeebs.Apps.Host.Create(args);
+log.Inf("Mileage Console app.");
