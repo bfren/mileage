ea05b90146bcd43581e40e1994f08958a19b4a19
Ben Green <ben@bcgdesign.com>
2022-03-24 17:16:44 +0000

    Adding MigrateToLatest command - #19

---
+++ src/Queries/MigrateToLatest/MigrateToLatestHandler.cs
@@ -0,0 +1,35 @@
+// Mileage Tracker
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using System.Threading;
+using System.Threading.Tasks;
+using Jeebs.Auth.Data;
+using Jeebs.Cqrs;
+using Jeebs.Data;
+using Jeebs.Logging;
+using Mileage.Persistence;
+
+namespace Mileage.Queries.MigrateToLatest;
+
+public sealed class MigrateToLatestHandler : ICommandHandler<MigrateToLatestCommand>
+{
+	private IDb Db { get; init; }
+
+	private Migrator Migrator { get; init; }
+
+	private IAuthDb AuthDb { get; init; }
+
+	private ILog<MigrateToLatestHandler> Log { get; init; }
+
+	public MigrateToLatestHandler(IDb db, Migrator migrator, IAuthDb authDb, ILog<MigrateToLatestHandler> log) =>
+		(Db, Migrator, AuthDb, Log) = (db, migrator, authDb, log);
+
+	public Task<Maybe<bool>> HandleAsync(MigrateToLatestCommand query, CancellationToken cancellationToken)
+	{
+		Log.Dbg("Migrating Authentication database to the latest version.");
+		AuthDb.MigrateToLatest();
+
+		Log.Dbg("Migrating Mileage database to the latest version.");
+		return F.Some(Migrator.MigrateToLatest(Db.Config.ConnectionString)).AsTask;
+	}
+}
