7df38e03a7ac522b89dab43a3d302896c83a02e8
Ben Green <ben@bcgdesign.com>
2022-03-24 10:15:58 +0000

    Adding CreateUser query and tests - see #11

---
+++ src/Queries/CreateUser/CreateUserHandler.cs
@@ -0,0 +1,40 @@
+// Mileage Tracker
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using System.Threading;
+using System.Threading.Tasks;
+using Jeebs.Auth;
+using Jeebs.Auth.Data;
+using Jeebs.Cqrs;
+using Jeebs.Logging;
+
+namespace Mileage.Queries.CreateUser;
+
+/// <summary>
+/// Create a new user
+/// </summary>
+public sealed class CreateUserHandler : IQueryHandler<CreateUserQuery, AuthUserId>
+{
+	private ILog<CreateUserHandler> Log { get; init; }
+
+	private IAuthUserRepository User { get; init; }
+
+	/// <summary>
+	/// Inject dependencies
+	/// </summary>
+	/// <param name="user"></param>
+	/// <param name="log"></param>
+	public CreateUserHandler(IAuthUserRepository user, ILog<CreateUserHandler> log) =>
+		(User, Log) = (user, log);
+
+	/// <summary>
+	/// Create a new user from <paramref name="query"/>
+	/// </summary>
+	/// <param name="query"></param>
+	/// <param name="cancellationToken"></param>
+	public Task<Maybe<AuthUserId>> HandleAsync(CreateUserQuery query, CancellationToken cancellationToken)
+	{
+		Log.Dbg("Create User: {Query}", query with { Password = "** REDACTED **" });
+		return User.CreateAsync(query.EmailAddress, query.Password, query.Name);
+	}
+}
---
+++ tests/Tests.Queries/CreateUser/CreateUserHandler/HandleAsync_Tests.cs
@@ -0,0 +1,75 @@
+// Mileage Tracker: Unit Tests
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Jeebs.Auth;
+using Jeebs.Auth.Data;
+using Jeebs.Logging;
+
+namespace Mileage.Queries.CreateUser.CreateUserHandler_Tests;
+
+public class HandleAsync_Tests
+{
+	private static (IAuthUserRepository, ILog<CreateUserHandler>, CreateUserHandler) Setup()
+	{
+		var repo = Substitute.For<IAuthUserRepository>();
+		var log = Substitute.For<ILog<CreateUserHandler>>();
+		var handler = new CreateUserHandler(repo, log);
+
+		return (repo, log, handler);
+	}
+
+	public class Logs_To_Dbg
+	{
+		[Fact]
+		public async Task With_Query_Using_Redacted_Password()
+		{
+			// Arrange
+			var (_, log, handler) = Setup();
+			var query = new CreateUserQuery(Rnd.Str, Rnd.Str, Rnd.Str);
+
+			// Act
+			await handler.HandleAsync(query, CancellationToken.None);
+
+			// Assert
+			log.Received().Dbg("Create User: {Query}", query with { Password = "** REDACTED **" });
+		}
+	}
+
+	public class Calls_Repo_CreateAsync
+	{
+		[Fact]
+		public async void With_Correct_Values()
+		{
+			// Arrange
+			var (repo, _, handler) = Setup();
+			var name = Rnd.Str;
+			var email = Rnd.Str;
+			var password = Rnd.Str;
+			var query = new CreateUserQuery(name, email, password);
+
+			// Act
+			await handler.HandleAsync(query, CancellationToken.None);
+
+			// Assert
+			await repo.Received().CreateAsync(email, password, name);
+		}
+
+		[Fact]
+		public async void Returns_Result()
+		{
+			// Arrange
+			var (repo, _, handler) = Setup();
+			var expected = new AuthUserId(Rnd.Lng);
+			repo.CreateAsync(default!, default!, default)
+				.ReturnsForAnyArgs(expected);
+			var query = new CreateUserQuery(Rnd.Str, Rnd.Str, Rnd.Str);
+
+			// Act
+			var result = await handler.HandleAsync(query, CancellationToken.None);
+
+			// Assert
+			var some = result.AssertSome();
+			Assert.Equal(expected, some);
+		}
+	}
+}
