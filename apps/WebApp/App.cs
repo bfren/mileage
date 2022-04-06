06269862b2900e32991b2f1c338e679455f4b506
bfren <ben@bcgdesign.com>
2022-04-06 11:42:17 +0100

    Creating WebApp - #37

---
+++ apps/WebApp/App.cs
@@ -0,0 +1,10 @@
+// Mileage Tracker Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Jeebs.Apps.Web;
+
+namespace Mileage.WebApp;
+
+public sealed class App : RazorApp
+{
+}
---
+++ apps/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+// Mileage Tracker Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mileage.WebApp.Pages;
+
+public class IndexModel : PageModel
+{
+	public void OnGet()
+	{
+	}
+}
