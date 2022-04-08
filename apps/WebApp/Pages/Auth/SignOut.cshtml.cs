83d33ec84d006d94c834e07ce577f1737454d9e7
bfren <ben@bcgdesign.com>
2022-04-08 09:08:35 +0100

    Updating sign in / out pages - #42

---
+++ apps/WebApp/Pages/Auth/SignOut.cshtml.cs
@@ -0,0 +1,23 @@
+// Mileage Tracker Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Jeebs.Mvc;
+using Jeebs.Mvc.Auth.Functions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mileage.WebApp.Pages.Auth;
+
+public class SignOutModel : PageModel
+{
+	public async Task<IActionResult> OnGet()
+	{
+		_ = await AuthF.DoSignOutAsync(new(
+			TempData.AddInfoAlert,
+			HttpContext.SignOutAsync
+		));
+
+		return RedirectToPage("/index");
+	}
+}
