e9ab6292a3d2b727dee4632454f2bf42792224c8
bfren <ben@bcgdesign.com>
2022-04-11 21:38:01 +0100

    Adding modal partial pages for settings - #49

---
+++ apps/WebApp/Pages/Settings/EditDefaultCar.cshtml.cs
@@ -0,0 +1,86 @@
+// Mileage Tracker Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Jeebs.Cqrs;
+using Jeebs.Logging;
+using Jeebs.Messages;
+using Jeebs.Mvc.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Mileage.Domain.GetCarsForUser;
+using Mileage.Domain.SaveSettings;
+using Mileage.WebApp.Pages.Partials;
+
+namespace Mileage.WebApp.Pages.Settings;
+
+[ValidateAntiForgeryToken]
+public sealed class EditDefaultCarModel : EditModalModel
+{
+	public Persistence.Common.Settings Settings { get; set; } = new();
+
+	public List<GetCarsForUserModel> Cars { get; set; } = new();
+
+	public IDispatcher Dispatcher { get; }
+
+	public ILog<EditDefaultCarModel> Log { get; }
+
+	public EditDefaultCarModel(IDispatcher dispatcher, ILog<EditDefaultCarModel> log) : base("Default Car") =>
+		(Dispatcher, Log) = (dispatcher, log);
+
+	public async Task<IActionResult> OnGetAsync()
+	{
+		Log.Vrb("Getting settings for {User}.", User.GetUserId());
+
+		// Get settings for the current user
+		var query = from u in User.GetUserId()
+					from s in Dispatcher.DispatchAsync(new Domain.LoadSettings.LoadSettingsQuery(u))
+					from c in Dispatcher.DispatchAsync(new GetCarsForUserQuery(u))
+					select new
+					{
+						Settings = s,
+						Cars = c
+					};
+
+		// Return page
+		return (await query)
+			.Switch<IActionResult>(
+				some: x =>
+				{
+					Settings = x.Settings;
+					Cars = x.Cars.ToList();
+					return Page();
+				},
+				none: r => Partial("ErrorModal", r)
+			);
+	}
+
+	public async Task<IActionResult> OnPostAsync(SaveSettingsCommand form)
+	{
+		Log.Vrb("Saving {Settings} for {User}.", form.Settings, User.GetUserId());
+
+		var saveCar = from u in User.GetUserId()
+					  from r in Dispatcher.DispatchAsync(form with { UserId = u })
+					  select r;
+
+		var result = await saveCar;
+		return result
+			.Audit(
+				none: Log.Msg
+			)
+			.Switch<IActionResult>(
+				some: x => x switch
+				{
+					true =>
+						ViewComponent("Car", new { CarId = form.Settings.DefaultCarId }),
+
+					false =>
+						new EmptyResult()
+				},
+				none: _ => new EmptyResult()
+			);
+	}
+
+	public static class M
+	{
+		public sealed record class UnableToSaveDefaultCarMsg : Msg;
+	}
+}
---
+++ apps/WebApp/Pages/Settings/EditDefaultFromPlace.cshtml.cs
@@ -0,0 +1,83 @@
+// Mileage Tracker Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Jeebs.Cqrs;
+using Jeebs.Logging;
+using Jeebs.Messages;
+using Jeebs.Mvc.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Mileage.Domain.GetPlacesForUser;
+using Mileage.Domain.SaveSettings;
+using Mileage.WebApp.Pages.Partials;
+
+namespace Mileage.WebApp.Pages.Settings;
+
+public sealed class EditDefaultFromPlaceModel : EditModalModel
+{
+	public Persistence.Common.Settings Settings { get; set; } = new();
+
+	public List<GetPlacesForUserModel> Places { get; set; } = new();
+
+	public IDispatcher Dispatcher { get; }
+
+	public ILog<EditDefaultFromPlaceModel> Log { get; }
+
+	public EditDefaultFromPlaceModel(IDispatcher dispatcher, ILog<EditDefaultFromPlaceModel> log) : base("Default Starting Place") =>
+		(Dispatcher, Log) = (dispatcher, log);
+
+	public async Task<IActionResult> OnGetAsync()
+	{
+		// Get settings for the current user
+		var query = from u in User.GetUserId()
+					from s in Dispatcher.DispatchAsync(new Domain.LoadSettings.LoadSettingsQuery(u))
+					from p in Dispatcher.DispatchAsync(new GetPlacesForUserQuery(u))
+					select new
+					{
+						Settings = s,
+						Places = p
+					};
+
+		// Return page
+		return (await query)
+			.Switch<IActionResult>(
+				some: x =>
+				{
+					Settings = x.Settings;
+					Places = x.Places.ToList();
+					return Page();
+				},
+				none: r => Partial("ErrorModal", r)
+			);
+	}
+
+	public async Task<IActionResult> OnPostAsync(SaveSettingsCommand form)
+	{
+		Log.Vrb("Saving {Settings} for {User}.", form.Settings, User.GetUserId());
+
+		var saveFromPlace = from u in User.GetUserId()
+							from r in Dispatcher.DispatchAsync(form with { UserId = u })
+							select r;
+
+		var result = await saveFromPlace;
+		return result
+			.Audit(
+				none: Log.Msg
+			)
+			.Switch<IActionResult>(
+				some: x => x switch
+				{
+					true =>
+						ViewComponent("Place", new { PlaceId = form.Settings.DefaultFromPlaceId }),
+
+					false =>
+						new EmptyResult()
+				},
+				none: _ => new EmptyResult()
+			);
+	}
+
+	public static class M
+	{
+		public sealed record class UnableToSaveDefaultFromPlaceMsg : Msg;
+	}
+}
