5ba88a5f6a04a18c6c541f7e9d70fe87d5ec3681
bfren <ben@bcgdesign.com>
2022-04-12 17:57:17 +0100

    Adding modal list search - #49

--- apps/WebApp/wwwroot/js/modal.inc.js
+++ apps/WebApp/wwwroot/js/modal.inc.js
@@ -1,3 +1,7 @@
+/**
+ * Setup token links to open the edit modal when clicked.
+ *
+ */
 function setupTokenModals() {
 	$(".token > a").click(function () {
 		var editUrl = $(this).data("edit");
@@ -9,6 +13,12 @@ ready(setupTokenModals);
 
 var modal;
 
+/**
+ * Open the edit modal and load contents via HTTP.
+ * 
+ * @param {any} url
+ * @param {any} replaceId
+ */
 function openModal(url, replaceId) {
 	// show messages
 	showPleaseWaitAlert();
@@ -25,25 +35,58 @@ function openModal(url, replaceId) {
 		var modalId = $(this).find(".modal").attr("id");
 
 		var modalEl = document.getElementById(modalId)
-		modal = new bootstrap.Modal(modalEl);
 
-		// fade in background and show modal
-		wrapper.fadeIn("fast", function () {
-			closeAlert();
+		// select item when modal is opened
+		modalEl.addEventListener("shown.bs.modal", function () {
+			$(".modal-select").focus();
 		});
-		modal.show();
-
-		// setup save button and ajax submit
-		setupModalSave();
-		setupAjaxSubmit();
 
 		// fade out background when modal is closed
 		modalEl.addEventListener("hide.bs.modal", function () {
 			wrapper.fadeOut("fast");
 		});
+
+		// fade in background and show modal
+		wrapper.fadeIn("fast", function () {
+			closeAlert();
+		});
+
+		modal = new bootstrap.Modal(modalEl);
+		modal.show();
+
+		// Setup save and submit
+		setupModalSearch();
+		setupModalSave();
+		setupAjaxSubmit();
 	});
 }
 
+/**
+ * Filter list elements based on search field entry.
+ *
+ */
+function setupModalSearch() {
+	$("#list-filter").keydown(function (e) {
+		// ignore enter key
+		if (e.keyCode == 13) {
+			e.preventDefault();
+			return;
+		}
+
+		// get value from input
+		var value = $(this).val().toLowerCase();
+
+		// filter items that match the input value
+		$("#list-items .list-item").filter(function () {
+			$(this).toggle($(this).data("text").indexOf(value) > -1);
+		});
+	});
+}
+
+/**
+ * Submit modal edit form when the save button is pressed.
+ *
+ */
 function setupModalSave() {
 	$("#edit .btn-save").click(function () {
 		$("#edit form").submit();
