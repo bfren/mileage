390958b68b65500807ac7668ea5fccf2fd86624b
bfren <ben@bcgdesign.com>
2022-04-13 15:39:32 +0100

    Moving modal layout / partials to 'Modals' directory - #49

--- apps/WebApp/Pages/Modals/EditModal.cshtml.cs
+++ apps/WebApp/Pages/Modals/EditModal.cshtml.cs
@@ -3,11 +3,11 @@
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Mileage.WebApp.Pages.Partials;
+namespace Mileage.WebApp.Pages.Modals;
 
 public abstract class EditModalModel : PageModel
 {
-	public string Title { get; init; }
+	public string Title { get; set; }
 
 	protected EditModalModel(string title) =>
 		Title = title;
