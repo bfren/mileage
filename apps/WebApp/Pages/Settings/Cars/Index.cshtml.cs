60a7b30b7aba986619f883b546291b3430a035db
bfren <ben@bcgdesign.com>
2022-04-14 10:27:05 +0100

    Adding cache profiles / auth to settings pages - #38

--- apps/WebApp/Pages/Settings/Cars/Index.cshtml.cs
+++ apps/WebApp/Pages/Settings/Cars/Index.cshtml.cs
@@ -1,10 +1,19 @@
 // Mileage Tracker Apps
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
+using Jeebs.Apps.Web.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mileage.WebApp.Pages.Settings.Cars;
 
+#if DEBUG
+[ResponseCache(CacheProfileName = CacheProfiles.None)]
+#else
+[ResponseCache(CacheProfileName = CacheProfiles.Default)]
+#endif
+[Authorize]
 public class IndexModel : PageModel
 {
 	public void OnGet()
