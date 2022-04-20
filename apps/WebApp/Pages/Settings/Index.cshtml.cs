5a264751dc2a0bfdd458d98481cdb5176d579174
bfren <ben@bcgdesign.com>
2022-04-20 17:46:31 +0100

    Don't try to load default settings on page load - #38

--- apps/WebApp/Pages/Settings/Index.cshtml.cs
+++ apps/WebApp/Pages/Settings/Index.cshtml.cs
@@ -1,23 +1,10 @@
 // Mileage Tracker Apps
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
-using Jeebs.Apps.Web.Constants;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mileage.WebApp.Pages.Settings;
 
-#if DEBUG
-[ResponseCache(CacheProfileName = CacheProfiles.None)]
-#else
-[ResponseCache(CacheProfileName = CacheProfiles.Default)]
-#endif
 [Authorize]
-public sealed class IndexModel : PageModel
-{
-	public void OnGet()
-	{
-
-	}
-}
+public sealed class IndexModel : PageModel { }
