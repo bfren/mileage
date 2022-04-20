277352c8f1210fccaf8ff26c002b26565545a6c1
bfren <ben@bcgdesign.com>
2022-04-20 17:45:55 +0100

    Removing caching - #38

--- apps/WebApp/Pages/Settings/Places/Index.cshtml.cs
+++ apps/WebApp/Pages/Settings/Places/Index.cshtml.cs
@@ -1,19 +1,14 @@
 // Mileage Tracker Apps
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
-using Jeebs.Apps.Web.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Mileage.WebApp.Pages.Settings.Places;
 
-#if DEBUG
-[ResponseCache(CacheProfileName = CacheProfiles.None)]
-#else
-[ResponseCache(CacheProfileName = CacheProfiles.Default)]
-#endif
 [Authorize]
+[ValidateAntiForgeryToken]
 public sealed class IndexModel : PageModel
 {
 	public void OnGet()
