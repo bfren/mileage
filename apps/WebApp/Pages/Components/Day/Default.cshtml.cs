77753a9ff197fbc8d5471f433a1c65bcdcca04c4
bfren <ben@bcgdesign.com>
2022-04-21 15:36:36 +0100

    Reformatting components - #41

--- apps/WebApp/Pages/Components/Day/Default.cshtml.cs
+++ apps/WebApp/Pages/Components/Day/Default.cshtml.cs
@@ -6,14 +6,10 @@ using Mileage.Persistence.Common.StrongIds;
 
 namespace Mileage.WebApp.Pages.Components.Day;
 
-public sealed record class DayModel(string? EditUrl, DateTime Day, JourneyId? JourneyId)
-{
-	public static DayModel Blank(string? editUrl) =>
-		new(editUrl, DateTime.Today, null);
-}
+public sealed record class DayModel(string? EditUrl, DateTime Day, JourneyId JourneyId);
 
 public sealed class DayViewComponent : ViewComponent
 {
-	public async Task<IViewComponentResult> InvokeAsync(string editUrl, DateTime day, JourneyId? journeyId) =>
+	public async Task<IViewComponentResult> InvokeAsync(string editUrl, DateTime day, JourneyId journeyId) =>
 		await Task.FromResult(View(new DayModel(editUrl, day, journeyId)));
 }
