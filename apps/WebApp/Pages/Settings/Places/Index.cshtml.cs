24f91c378213c1080450af721ee05ef7990766e9
bfren <ben@bcgdesign.com>
2022-04-21 08:44:19 +0100

    Adding places CRUD partials - #39

--- apps/WebApp/Pages/Settings/Places/Index.cshtml.cs
+++ apps/WebApp/Pages/Settings/Places/Index.cshtml.cs
@@ -1,17 +1,41 @@
 // Mileage Tracker Apps
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
+using Jeebs.Cqrs;
+using Jeebs.Logging;
+using Jeebs.Mvc.Auth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mileage.Domain.GetPlaces;
 
 namespace Mileage.WebApp.Pages.Settings.Places;
 
 [Authorize]
 [ValidateAntiForgeryToken]
-public sealed class IndexModel : PageModel
+public sealed partial class IndexModel : PageModel
 {
-	public void OnGet()
+	private IDispatcher Dispatcher { get; }
+
+	private ILog<IndexModel> Log { get; }
+
+	public List<GetPlacesModel> Places { get; set; } = new();
+
+	public IndexModel(IDispatcher dispatcher, ILog<IndexModel> log) =>
+		(Dispatcher, Log) = (dispatcher, log);
+
+	public async Task<IActionResult> OnGetAsync()
 	{
+		var query = from u in User.GetUserId()
+					from p in Dispatcher.DispatchAsync(new GetPlacesQuery(u))
+					select p;
+
+		await foreach (var places in query)
+		{
+			Places = places.ToList();
+			return Page();
+		}
+
+		return new NoContentResult();
 	}
 }
