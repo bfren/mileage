c46de727e2011629daade411b2f55c39e8cda445
bfren <ben@bcgdesign.com>
2022-04-21 10:04:31 +0100

    Adding add / edit Rate form - #40

--- apps/WebApp/Pages/Settings/Rates/Index.cshtml.cs
+++ apps/WebApp/Pages/Settings/Rates/Index.cshtml.cs
@@ -1,17 +1,40 @@
 // Mileage Tracker Apps
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
+using Jeebs.Cqrs;
+using Jeebs.Logging;
+using Jeebs.Mvc.Auth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mileage.Domain.GetRates;
 
 namespace Mileage.WebApp.Pages.Settings.Rates;
 
 [Authorize]
 [ValidateAntiForgeryToken]
-public sealed class IndexModel : PageModel
+public sealed partial class IndexModel : PageModel
 {
-	public void OnGet()
+	private IDispatcher Dispatcher { get; }
+
+	private ILog<IndexModel> Log { get; }
+
+	public List<GetRatesModel> Rates { get; set; } = new();
+
+	public IndexModel(IDispatcher dispatcher, ILog<IndexModel> log) =>
+		(Dispatcher, Log) = (dispatcher, log);
+
+	public async Task<IActionResult> OnGetAsync()
 	{
+		var query = from u in User.GetUserId()
+					from r in Dispatcher.DispatchAsync(new GetRatesQuery(u))
+					select r;
+
+		await foreach (var rates in query)
+		{
+			Rates = rates.ToList();
+		}
+
+		return Page();
 	}
 }
---
+++ apps/WebApp/Pages/Settings/Rates/_Form.cshtml.cs
@@ -0,0 +1,49 @@
+// Mileage Tracker Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using Jeebs.Mvc;
+using Jeebs.Mvc.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Mileage.Domain.GetRate;
+using Mileage.Domain.SaveRate;
+using Mileage.Persistence.Common.StrongIds;
+
+namespace Mileage.WebApp.Pages.Settings.Rates;
+
+public sealed partial class IndexModel
+{
+	public async Task<PartialViewResult> OnGetFormPartialAsync(RateId? rateId)
+	{
+		// Return blank form
+		if (rateId is null)
+		{
+			return Partial("_Form");
+		}
+
+		// Create query
+		var query = from u in User.GetUserId()
+					from c in Dispatcher.DispatchAsync(new GetRateQuery(u, rateId))
+					select c;
+
+		return await query
+			.AuditAsync(none: Log.Msg)
+			.SwitchAsync(
+				some: x => Partial("_Form", x),
+				none: () => Partial("_Form")
+			);
+	}
+
+	public Task<IActionResult> OnPostFormPartialAsync(SaveRateQuery form)
+	{
+		var query = from u in User.GetUserId()
+					from r in Dispatcher.DispatchAsync(form with { UserId = u })
+					select r;
+
+		return query
+			.AuditAsync(none: Log.Msg)
+			.SwitchAsync(
+				some: _ => OnGetAsync(),
+				none: r => Result.Error(r)
+			);
+	}
+}
