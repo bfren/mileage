0415a9d47ca99e54b1714036d5f4aa456f89ae43
bfren <ben@bcgdesign.com>
2022-04-21 10:03:16 +0100

    Adding RateList view component - #40

---
+++ apps/WebApp/Pages/Components/List/RateList.cs
@@ -0,0 +1,13 @@
+// Mileage Tracker Apps
+// Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
+
+using System.Globalization;
+using Mileage.Domain.GetRates;
+using Mileage.Persistence.Common.StrongIds;
+
+namespace Mileage.WebApp.Pages.Components.List;
+
+public sealed class RateListViewComponent : ListViewComponent<GetRatesModel, RateId>
+{
+	public RateListViewComponent() : base("rate", x => x.AmountPerMileGBP.ToString("0.00", CultureInfo.InvariantCulture)) { }
+}
