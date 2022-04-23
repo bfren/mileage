49bc9fc474c9362aae7fceea36bd01f3844f9dc7
bfren <ben@bcgdesign.com>
2022-04-23 14:50:38 +0100

    Adding validation to create journey partial - #41

--- apps/WebApp/Pages/Journeys/_EditToPlaces.cshtml.cs
+++ apps/WebApp/Pages/Journeys/_EditToPlaces.cshtml.cs
@@ -12,7 +12,7 @@ public sealed class EditToPlacesModel : EditJourneyModalModel
 {
 	public List<GetPlacesModel> Places { get; set; } = new();
 
-	public List<PlaceId> Selected { get; set; } = new();
+	public PlaceId[] Selected { get; set; } = Array.Empty<PlaceId>();
 
 	public EditToPlacesModel() : base("Destinations") { }
 }
@@ -22,7 +22,7 @@ public sealed partial class IndexModel
 	public Task<PartialViewResult> OnGetEditToPlacesAsync(JourneyId journeyId) =>
 		GetFieldAsync("ToPlaces", journeyId,
 			x => Dispatcher.DispatchAsync(new GetPlacesQuery(x)),
-			(j, v) => new EditToPlacesModel { Journey = j, Places = v.ToList(), Selected = j.ToPlaceIds.ToList() }
+			(j, v) => new EditToPlacesModel { Journey = j, Places = v.ToList(), Selected = j.ToPlaceIds.ToArray() }
 		);
 
 	public Task<IActionResult> OnPostEditToPlacesAsync(UpdateJourneyToPlacesCommand journey) =>
