5fa56b3ff8a03bfeaae458849edeab1fca47511d
bfren <ben@bcgdesign.com>
2022-04-23 15:33:39 +0100

    Adding recent journeys to home page - #66

--- apps/WebApp/Pages/Index.cshtml.cs
+++ apps/WebApp/Pages/Index.cshtml.cs
@@ -1,22 +1,17 @@
 // Mileage Tracker Apps
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
-using Jeebs.Apps.Web.Constants;
 using Jeebs.Cqrs;
 using Jeebs.Mvc.Auth;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Mileage.Domain.GetIncompleteJourneys;
+using Mileage.Domain.GetRecentJourneys;
 using Mileage.WebApp.Pages.Journeys;
 
 namespace Mileage.WebApp.Pages;
 
-#if DEBUG
-[ResponseCache(CacheProfileName = CacheProfiles.None)]
-#else
-[ResponseCache(CacheProfileName = CacheProfiles.Default)]
-#endif
 [Authorize]
 public sealed class IndexModel : PageModel
 {
@@ -24,18 +19,22 @@ public sealed class IndexModel : PageModel
 
 	public IncompleteModel IncompleteJourneys { get; private set; } = new();
 
+	public RecentModel RecentJourneys { get; private set; } = new();
+
 	public IndexModel(IDispatcher dispatcher) =>
 		Dispatcher = dispatcher;
 
 	public async Task<IActionResult> OnGetAsync()
 	{
 		var query = from u in User.GetUserId()
-					from j in Dispatcher.DispatchAsync(new GetIncompleteJourneysQuery(u))
-					select j;
+					from incomplete in Dispatcher.DispatchAsync(new GetIncompleteJourneysQuery(u))
+					from recent in Dispatcher.DispatchAsync(new GetRecentJourneysQuery(u))
+					select new { incomplete, recent };
 
-		await foreach (var journeys in query)
+		await foreach (var j in query)
 		{
-			IncompleteJourneys = new() { Journeys = journeys.ToList() };
+			IncompleteJourneys = new() { Journeys = j.incomplete.ToList() };
+			RecentJourneys = new() { Journeys = j.recent.ToList() };
 			return Page();
 		}
 
