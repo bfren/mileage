efd688b5ec9b4557e19fb4f40ca7e156c0948ed0
bfren <ben@bcgdesign.com>
2022-04-24 18:06:31 +0100

    Use loadPage when handling results - #70

--- apps/WebApp/wwwroot/js/result.inc.js
+++ apps/WebApp/wwwroot/js/result.inc.js
@@ -4,12 +4,11 @@
  * @param {any} r
  */
 function handleResult(r) {
-	// show alert
-	showAlert(r.message.type, r.message.text);
-
-	// redirect
+	// redirect or show alert
 	if (r.redirectTo) {
-		showAlert(alertTypes.info, "Redirecting...", true);
-		window.location.href = r.redirectTo;
+		loadPage(r.redirectTo);
+	} else {
+		// show alert
+		showAlert(r.message.type, r.message.text);
 	}
 }
