a161b1dba24b4e82fec386d3ecfe4593e86ff318
bfren <ben@bcgdesign.com>
2022-04-24 15:00:27 +0100

    Adding XML comments - #70

--- src/Domain/WithUserId.cs
+++ src/Domain/WithUserId.cs
@@ -5,7 +5,13 @@ using Jeebs.Auth.Data;
 
 namespace Mileage.Domain;
 
+/// <summary>
+/// Specifies a class has a 'UserId' property
+/// </summary>
 public abstract record class WithUserId
 {
+	/// <summary>
+	/// User ID
+	/// </summary>
 	public abstract AuthUserId UserId { get; init; }
 }
