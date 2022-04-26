64f7a3989d15c4aaba66ddbdaaf9483f3a8da8f7
bfren <ben@bcgdesign.com>
2022-04-26 14:43:53 +0100

    Adding get expense report data migration - #67

--- src/Persistence.Common/Constants.cs
+++ src/Persistence.Common/Constants.cs
@@ -14,6 +14,11 @@ public static class Constants
 	public static string Schema { get; } = "mileage";
 
 	/// <summary>
+	/// Default number of months to generate expense reports for
+	/// </summary>
+	public static int ExpensesReportMonths { get; } = 6;
+
+	/// <summary>
 	/// Function names
 	/// </summary>
 	public static class Functions
@@ -22,5 +27,10 @@ public static class Constants
 		/// Get expenses report recent months
 		/// </summary>
 		public static string GetExpensesReportRecentMonths { get; } = $"{Schema}.get_expenses_report_recent_months";
+
+		/// <summary>
+		/// Get expenses report data
+		/// </summary>
+		public static string GetExpensesReportData { get; } = $"{Schema}.get_expenses_report_data";
 	}
 }
