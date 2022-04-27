f1fffee4907dc6d95dafd8f6645375372f678349
bfren <ben@bcgdesign.com>
2022-04-27 14:24:35 +0100

    Adding XML comments - #84

--- src/Persistence.Common/Reports/ExpensesReportJourney.cs
+++ src/Persistence.Common/Reports/ExpensesReportJourney.cs
@@ -7,23 +7,53 @@ using Mileage.Persistence.Common.StrongIds;
 
 namespace Mileage.Persistence.Common.Reports;
 
+/// <summary>
+/// Expenses Report: Journey model
+/// </summary>
 public sealed record class ExpensesReportJourney
 {
+	/// <summary>
+	/// Journey ID
+	/// </summary>
 	public JourneyId Id { get; init; } = new();
 
+	/// <summary>
+	/// Journey Day
+	/// </summary>
 	public DateTime Day { get; init; }
 
+	/// <summary>
+	/// Journey Start Miles
+	/// </summary>
 	public int Start { get; init; }
 
+	/// <summary>
+	/// Journey End Miles
+	/// </summary>
 	public int End { get; init; }
 
+	/// <summary>
+	/// Journey calculated distance (null if <see cref="End"/> is zero)
+	/// </summary>
 	public int? Distance { get; init; }
 
+	/// <summary>
+	/// Journey Car number plate
+	/// </summary>
 	public string Car { get; init; } = string.Empty;
 
+	/// <summary>
+	/// Journey Rate amount
+	/// </summary>
 	public float Rate { get; init; }
 
+	/// <summary>
+	/// Journey Starting Place
+	/// </summary>
 	public ExpensesReportPlace From { get; init; } = new();
 
+	/// <summary>
+	/// Journey Destinations
+	/// </summary>
 	public List<ExpensesReportPlace> To { get; init; } = new();
 }
--- src/Persistence.Common/Reports/ExpensesReportPlace.cs
+++ src/Persistence.Common/Reports/ExpensesReportPlace.cs
@@ -3,9 +3,18 @@
 
 namespace Mileage.Persistence.Common.Reports;
 
+/// <summary>
+/// Expenses Report: Place model
+/// </summary>
 public sealed record class ExpensesReportPlace
 {
+	/// <summary>
+	/// Place Description
+	/// </summary>
 	public string Description { get; init; } = string.Empty;
 
+	/// <summary>
+	/// Place Postcode
+	/// </summary>
 	public string? Postcode { get; init; }
 }
