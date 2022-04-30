6b5d7141721091de7f0e401ef189426d265de072
bfren <ben@bcgdesign.com>
2022-04-30 08:31:55 +0100

    Adding Car Delete / Disable tests - #92

--- tests/Tests.Domain/DeleteCar/DeleteCarHandler/HandleAsync_Tests.cs
+++ tests/Tests.Domain/DeleteCar/DeleteCarHandler/HandleAsync_Tests.cs
@@ -2,21 +2,21 @@
 // Copyright (c) bfren - licensed under https://mit.bfren.dev/2022
 
 using Jeebs.Auth.Data;
-using Mileage.Domain.DeleteCar.Messages;
+using Mileage.Domain.CheckCarCanBeDeleted;
 using Mileage.Persistence.Common.StrongIds;
 using Mileage.Persistence.Entities;
 using Mileage.Persistence.Repositories;
 
 namespace Mileage.Domain.DeleteCar.DeleteCarHandler_Tests;
 
-public class HandleAsync_Tests : Abstracts.Delete.HandleAsync_Tests
+public class HandleAsync_Tests : Abstracts.DeleteOrDisable.HandleAsync_Tests
 {
-	private class Setup : Delete_Setup<ICarRepository, CarEntity, CarId, DeleteCarCommand, DeleteCarHandler, CarToDelete>
+	private class Setup : Setup<ICarRepository, CarEntity, CarId, DeleteCarCommand, DeleteCarHandler, CarToDelete, CheckCarCanBeDeletedQuery>
 	{
 		public Setup() : base("Car") { }
 
 		internal override DeleteCarHandler GetHandler(Vars v) =>
-			new(v.Repo, v.Log);
+			new(v.Cache, v.Repo, v.Dispatcher, v.Log);
 
 		internal override DeleteCarCommand GetCommand(AuthUserId? userId = null, CarId? entityId = null)
 		{
@@ -28,41 +28,42 @@ public class HandleAsync_Tests : Abstracts.Delete.HandleAsync_Tests
 			return new(LongId<AuthUserId>(), LongId<CarId>());
 		}
 
-		internal override CarToDelete EmptyModel { get; } = new(LongId<CarId>(), Rnd.Lng);
+		internal override CarToDelete EmptyModel { get; } = new(LongId<CarId>(), Rnd.Lng, Rnd.Flip);
 	}
 
 	[Fact]
 	public override async Task Test00_Calls_Log_Vrb__With_Query()
 	{
-		await new Setup().Test00((q, c) => q.HandleAsync(c));
+		await new Setup().Test00((h, c, d) => h.HandleAsync(c, d));
 	}
 
 	[Fact]
-	public override async Task Test01_Calls_FluentQuery_Where__With_Correct_Values()
+	public override async Task Test01_Dispatches_Check_Item_Can_Be_Deleted_Query__With_Correct_Values()
 	{
-		await new Setup().Test01((q, c) => q.HandleAsync(c));
+		await new Setup().Test01((h, c, d) => h.HandleAsync(c, d));
 	}
 
 	[Fact]
-	public override async Task Test02_Calls_FluentQuery_WhereSingleAsync__Receives_None__Audits_Msg()
+	public override async Task Test02_Calls_Delete_Or_Disable__Receives_Some_True__Removes_Item_From_Cache()
 	{
-		await new Setup().Test02((q, c) => q.HandleAsync(c));
+		await new Setup().Test02((h, c, d) => h.HandleAsync(c, d));
 	}
 
 	[Fact]
-	public override async Task Test03_Calls_FluentQuery_WhereSingleAsync__Receives_None__Returns_None_With_DoesNotExistMsg()
+	public override async Task Test03_Calls_Delete_Or_Disable__Receives_Some_False__Leaves_Cache_Alone()
 	{
-		await new Setup().Test03<CarDoesNotExistMsg>(msg => msg.UserId, msg => msg.CarId, (q, c) => q.HandleAsync(c));
+		await new Setup().Test03((h, c, d) => h.HandleAsync(c, d));
 	}
 
 	[Fact]
-	public override async Task Test04_Calls_FluentQuery_WhereSingleAsync__Receives_Some__Calls_Repo_DeleteAsync__With_Correct_Value()
+	public override async Task Test04_Calls_Delete_Or_Disable__Receives_None__Leaves_Cache_Alone()
 	{
-		await new Setup().Test04((carId, version) => new(carId, version), (q, c) => q.HandleAsync(c));
+		await new Setup().Test04((h, c, d) => h.HandleAsync(c, d));
 	}
 
-	public override async Task Test05_Calls_FluentQuery_WhereSingleAsync__Receives_Some__Calls_Repo_DeleteAsync__Returns_Result()
+	[Fact]
+	public override async Task Test05_Calls_Delete_Or_Disable__Returns_Result()
 	{
-		await new Setup().Test05((q, c) => q.HandleAsync(c));
+		await new Setup().Test05((h, c, d) => h.HandleAsync(c, d));
 	}
 }
