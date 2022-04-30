78d44e745be64b3fd627e5dae7a61b2000d4d6cd
bfren <ben@bcgdesign.com>
2022-04-30 11:08:00 +0100

    Adding disabled support to rate settings - #92

--- apps/WebApp/Pages/Settings/Rates/_Delete.cshtml.cs
+++ apps/WebApp/Pages/Settings/Rates/_Delete.cshtml.cs
@@ -4,8 +4,11 @@
 using Jeebs.Mvc;
 using Jeebs.Mvc.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Mileage.Domain.CheckRateCanBeDeleted;
+using Mileage.Domain.CheckRateCanBeDeleted.Messages;
 using Mileage.Domain.DeleteRate;
 using Mileage.Domain.GetRate;
+using Mileage.Persistence.Common;
 using Mileage.Persistence.Common.StrongIds;
 using Mileage.WebApp.Pages.Modals;
 
@@ -23,15 +26,27 @@ public sealed partial class IndexModel
 	public Task<PartialViewResult> OnGetDeleteAsync(RateId rateId)
 	{
 		// Create query
-		var query = from u in User.GetUserId()
-					from c in Dispatcher.DispatchAsync(new GetRateQuery(u, rateId))
-					select c;
+		var query = from userId in User.GetUserId()
+					from op in Dispatcher.DispatchAsync(new CheckRateCanBeDeletedQuery(userId, rateId))
+					from rate in Dispatcher.DispatchAsync(new GetRateQuery(userId, rateId))
+					select new { op, rate };
 
 		return query
 			.AuditAsync(none: Log.Msg)
 			.SwitchAsync(
-				some: x => Partial("_Delete", new DeleteModel { Rate = x }),
-				none: r => Partial("Modals/ErrorModal", r)
+				some: x => Partial("_Delete", new DeleteModel { Rate = x.rate, Operation = x.op }),
+				none: r => r switch
+				{
+					RateIsDefaultRateMsg =>
+						Partial("_Delete", new DeleteModel
+						{
+							Operation = DeleteOperation.None,
+							Reason = "it is the default rate for new journeys"
+						}),
+
+					_ =>
+						Partial("Modals/ErrorModal", r)
+				}
 			);
 	}
 
